8abd7a745a000c9b03b26b404a69f95648848b30
Aslam youseph <aslamyouseph@gmail.com>
2025-01-14 19:54:49 +0530

    Frondend and backend connection esstablished succesfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from Node.js server
+    axios
+      .get("http://localhost:5000")
+      .then((response) => {
+        setData(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello Aslam </h1>
+    <div>
+      <h1>React and Node.js Connection</h1>
+      {data ? <p>{data}</p> : <p>Loading...</p>}
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 5000; // This project is run on the port number 5000
+
+// // Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, it's Aslam Youseph from the server side!" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
