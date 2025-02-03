9688612dd6a127972612a0800a7f996f9b68a79c
Aslam youseph <aslamyouseph@gmail.com>
2025-02-03 18:40:26 +0530

    Navigation link added to the componets

--- client/src/User-Components/Home/laptopsCategory/laptopCategory.js
+++ client/src/User-Components/Home/laptopsCategory/laptopCategory.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./laptopCategory.css";
+import { Link } from "react-router-dom"; // This is used to enable routing in react example (http://localhost:3000/page1)
+
 function laptopCategory() {
   return (
     <div>
@@ -18,61 +20,93 @@ function laptopCategory() {
       {/* Here onwards the card is starting */}
       <div class="card-container">
         <div class="card">
-          <div class="card-content">
-            <h2>Gaming Laptops</h2>
-            <p>
-              Experience unbeatable performance and stunning visuals with our
-              high-performance gaming laptops, designed for gamers who demand
-              the best.
-            </p>
-          </div>
+          <Link
+            to="/gamingLaptops"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div class="card-content">
+              <h2>Gaming Laptops</h2>
+              <p>
+                Experience unbeatable performance and stunning visuals with our
+                high-performance gaming laptops, designed for gamers who demand
+                the best.
+              </p>
+            </div>
+          </Link>
         </div>
         <div class="card">
-          <div class="card-content">
-            <h2>Business Laptops</h2>
-            <p>
-              Power up your gaming experience with our high-performance laptops,
-              featuring top-tier processors, graphics, and immersive displays.
-            </p>
-          </div>
+          <Link
+            to="/BusinessLaptops"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div class="card-content">
+              <h2>Business Laptops</h2>
+              <p>
+                Power up your gaming experience with our high-performance
+                laptops, featuring top-tier processors, graphics, and immersive
+                displays.
+              </p>
+            </div>
+          </Link>
         </div>
         <div class="card">
-          <div class="card-content">
-            <h2>Budget Laptops</h2>
-            <p>
-              Get the best value for your money with our budget laptops,
-              offering reliable performance and essential features at an
-              affordable price.
-            </p>
-          </div>
+          <Link
+            to="/BudgetLaptops"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div class="card-content">
+              <h2>Budget Laptops</h2>
+              <p>
+                Get the best value for your money with our budget laptops,
+                offering reliable performance and essential features at an
+                affordable price.
+              </p>
+            </div>
+          </Link>
         </div>
         <div class="card">
-          <div class="card-content">
-            <h2>Student Laptops</h2>
-            <p>
-              Find the perfect student laptops, combining affordability,
-              performance, and portability to help you succeed in your studies
-            </p>
-          </div>
+          <Link
+            to="/StudentLaptops"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div class="card-content">
+              <h2>Student Laptops</h2>
+              <p>
+                Find the perfect student laptops, combining affordability,
+                performance, and portability to help you succeed in your studies
+              </p>
+            </div>
+          </Link>
         </div>
         <div class="card">
-          <div class="card-content">
-            <h2>Who need tablets</h2>
-            <p>
-              Tablets are perfect for those seeking a portable, versatile device
-              for work, study, entertainment, or creative tasks on the go
-            </p>
-          </div>
+          <Link
+            to="/Tablets"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div class="card-content">
+              <h2>Who need tablets</h2>
+              <p>
+                Tablets are perfect for those seeking a portable, versatile
+                device for work, study, entertainment, or creative tasks on the
+                go
+              </p>
+            </div>
+          </Link>
         </div>
         <div class="card">
-          <div class="card-content">
-            <h2>2 in one</h2>
-            <p>
-              2-in-1 laptops offer the flexibility of both a laptop and a tablet
-              in one device, perfect for those who need power and portability in
-              a single, versatile machine
-            </p>
-          </div>
+          <Link
+            to="/TwoInOne"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            <div class="card-content">
+              <h2>2 in one</h2>
+              <p>
+                2-in-1 laptops offer the flexibility of both a laptop and a
+                tablet in one device, perfect for those who need power and
+                portability in a single, versatile machine
+              </p>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
