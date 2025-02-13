53b39214b62117ab9f68f09cf30232b470c72d35
Aslam youseph <aslamyouseph@gmail.com>
2025-02-13 17:57:28 +0530

    Admin folder created

---
+++ client/src/Admin-Components/Admin-Home/AdminNavBar/AdminNavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AdminNavBar.css";
+function AdminNavBar() {
+  return (
+    <div>
+      <h1>It is the admin navigation bar</h1>
+    </div>
+  );
+}
+
+export default AdminNavBar;
---
+++ client/src/Admin-Components/Admin-page/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminNavBar from "../Admin-Home/AdminNavBar/AdminNavBar";
+function AdminHome() {
+  return (
+    <div>
+      {/*This is used to import the admin Navigation bar  form Admin folder */}
+      <AdminNavBar />
+    </div>
+  );
+}
+
+export default AdminHome;
